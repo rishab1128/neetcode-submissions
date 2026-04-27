7f9223364da67268d396f7d1d2fe98007e7cc488
Rishab Purkayastha <63293155+rishab1128@users.noreply.github.com>
2026-04-27 14:22:01 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int recur(vector<int>&arr, int idx, vector<int>&dp)
+    {
+        if(idx >= arr.size())
+            return 0;
+
+        if(dp[idx]!=0)
+            return dp[idx];
+
+        int op1 = arr[idx] + recur(arr,idx+2,dp);
+        int op2 = recur(arr,idx+1,dp);
+
+        return dp[idx] = max(op1,op2);
+    }
+    int rob(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int>dp(n,0);
+        int ans = recur(arr,0,dp);
+        return ans;
+    }
+};
